2bf6a459676ac78d386dbc9fab0cdb75e61b0337
Noni Vo <nonivhm@gmail.com>
2022-01-10 20:25:18 -0600

    Spring boot project starter

---
+++ src/test/java/com/hvo/tbclibrary/TbcLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvo.tbclibrary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TbcLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
